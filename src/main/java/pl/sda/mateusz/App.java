b8a9902c48879b1c5fffee6b9ca565c923a019c1
Mateusz Grzeskowiak <mateusz.r.grzeskowiak@gmail.com>
2020-02-02 13:56:04 +0100

    Added zodiac sign recogniser by date (to check)

--- src/main/java/pl/sda/mateusz/App.java
+++ src/main/java/pl/sda/mateusz/App.java
@@ -7,10 +7,14 @@ import java.util.Scanner;
 public class App
 {
     public static void main( String[] args ){
-        System.out.println( "Podaj znak zodiaku pisany w języku polskim, małymi literami" );
+        System.out.println( "Hej, jak masz na imię?" );
         Scanner scan = new Scanner(System.in);
-        String userInput = scan.nextLine();
-        String requestResult = Connector.getPage(userInput);
+        String userName = scan.nextLine();
+        String requestResult = Connector.getPage(userName);
+        System.out.println( "Którego dnia miesiąca się urodziłeś?");
+        Integer day = scan.nextInt();
+        System.out.println( "W którym miesiącu się urodziłeś? Podaj liczbę");
+        Integer month = scan.nextInt();
         String horoscopeMessage = "";
         if (!requestResult.equals(Connector.NOT_FOUND)){
             horoscopeMessage = HoroscopeExtractor.getHoroscopeText(requestResult);
