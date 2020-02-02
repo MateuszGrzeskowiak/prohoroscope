4e5c8cd600992d88a1d3448b0e1b9d7bf28f0fbe
Mateusz Grzeskowiak <mateusz.r.grzeskowiak@gmail.com>
2020-02-02 21:07:49 +0100

    Added zodiac sign recogniser by date (checked)

--- src/main/java/pl/sda/mateusz/App.java
+++ src/main/java/pl/sda/mateusz/App.java
@@ -10,11 +10,12 @@ public class App
         System.out.println( "Hej, jak masz na imię?" );
         Scanner scan = new Scanner(System.in);
         String userName = scan.nextLine();
-        String requestResult = Connector.getPage(userName);
-        System.out.println( "Którego dnia miesiąca się urodziłeś?");
+        System.out.println( userName + " którego dnia miesiąca się urodziłeś?");
         Integer day = scan.nextInt();
-        System.out.println( "W którym miesiącu się urodziłeś? Podaj liczbę");
+        System.out.println( userName + " w którym miesiącu się urodziłeś? Podaj liczbę");
         Integer month = scan.nextInt();
+        String sign = ZodiacSignRecogniser.getSign(day, month);
+        String requestResult = Connector.getPage(sign);
         String horoscopeMessage = "";
         if (!requestResult.equals(Connector.NOT_FOUND)){
             horoscopeMessage = HoroscopeExtractor.getHoroscopeText(requestResult);
